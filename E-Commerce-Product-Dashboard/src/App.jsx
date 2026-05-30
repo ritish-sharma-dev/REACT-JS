ca3c770f02820bd8b7af67a1cc3406d5ae49fbbf
Ritish Sharma <ritishsharma04022006@gmail.com>
2026-05-31 02:40:47 +0530

    ADD

--- E-Commerce-Product-Dashboard/src/App.jsx
+++ E-Commerce-Product-Dashboard/src/App.jsx
@@ -1,28 +1,73 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import ProductTable from './components/ProductTable'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [products, setProducts] = useState(undefined)
+  const [products, setProducts] = useState([])
+  const [filterProducts, setFilterProducts] = useState(products)
 
-  const fetchingData = async ()=>{
-    try{
-      const response = await fetch("https://fakestoreapi.com/products")
-      const products = await response.json()
-      setProducts(products)
-    } catch(error){
-      console.log("Failed to load products.")
-    }
+  let fetchingData = async () => {
+    let response = await fetch("https://fakestoreapi.com/products")
+    let p = await response.json()
+    setProducts(p)
+    setFilterProducts(p)
   }
 
-  useEffect( ()=>{
+  let filterHandler = (e)=>{
+    let filter;
+    if (e.target.value == "LowToHigh"){
+      filter = [...products].sort((a,b)=> a.price - b.price)
+    }else if (e.target.value == "HighToLow"){
+      filter = [...products].sort((a,b)=> b.price - a.price)
+    }else{
+      filter = products
+    }
+    console.log(filter)
+    setFilterProducts(filter)
+  }
+
+
+  useEffect(() => {
     fetchingData()
-  },[])
+  }, [])
+
+  useEffect(() => {
+    console.log(products)
+  }, [products])
 
   return (
     <div>
-      {products != undefined ? <ProductTable products={products}/> : "Loading....."}
+      <select onChange={filterHandler} name="" id="">
+        <option value="">---------</option>
+        <option value="LowToHigh">Low To High</option>
+        <option value="HighToLow">High To Low</option>
+      </select>
+      <table border={1} cellPadding={3} cellSpacing={3}>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>ProductName</td>
+            <td>Price</td>
+            <td>Category</td>
+            <td>Rating</td>
+            <td>Image</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            filterProducts.map((product, index) => {
+              return (
+                <tr key={index}>
+                  <td>{product.id}</td>
+                  <td>{product.title}</td>
+                  <td>{product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.rating.rate}</td>
+                  <img width={100} height={100} src={product.image} alt="" />
+                </tr>
+            )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
